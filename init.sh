5f1aa233988046ff5b56a3bf48e024c2063974e8
Alexa020204 <heartPink303@gmail.com>
2025-03-25 10:40:41 -0500

    Creacion del Proyecto 👍
    Aqui se me olvido cambiarme a una rama. JAJAJJA

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Nombre del proyecto (puedes modificarlo según tus necesidades)
+PROJECT_NAME="backend"
+
+
+npm init -y
+mkdir controllers models routers
+
+echo "Proyecto '$PROJECT_NAME' creado con éxito"
