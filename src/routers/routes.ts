2fcafedaa8a1c08406b4d44275469e169f6c1469
Alexa020204 <heartPink303@gmail.com>
2025-04-08 20:24:00 -0500

    Update routes.ts

--- src/routers/routes.ts
+++ src/routers/routes.ts
@@ -10,12 +10,14 @@ router.get('/', (req, res) => {
     res.send('¡Hola desde la ruta principal!');
   });
 
+
+router.post('/register', RegisterController.register);
 router.post('/login', LoginController.login);
 router.post('/officeConvert',  PDFController.officeConvert);
 router.post('/urlConvert', PDFController.urlConvert);
 router.post('/total', MetricsController.metricsTotal);
 router.post('/statistics', Authenticate.validate ,MetricsController.getStatistics);
-router.post('/register', RegisterController.register);
+
 
 export default router;
 
