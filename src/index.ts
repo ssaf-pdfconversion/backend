68814f6f47b959bb59396b5b61a2d93878b51a1a
Alexa020204 <heartPink303@gmail.com>
2025-04-14 12:14:54 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,11 @@ const port = 2424;
 // Middleware para parsear JSON
 app.use(express.json());
 
+
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
 // Ruta principal
 app.use('/', routes);
 
